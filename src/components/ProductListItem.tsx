01b93382e1e06a97a323797a6b899274c06c33b3
ebrardev <16155052@mersin.edu.tr>
2024-03-16 20:26:54 +0300

    Refactor ProductListItem component to use default image if product image is not available

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,14 +1,20 @@
 import { StyleSheet,Text,View,Image } from 'react-native';
 
+import { Product } from "../types";     
 
 import Colors from "../constants/Colors";
 
+export const defaultImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png"
 
-const ProductListItem = ({product}) => {    
+type ProductListItemProps = {
+    product: Product;
+}
+
+const ProductListItem = ({product}: ProductListItemProps) => {    
     return (
   
              <View style={styles.container}>
-      <Image source={{uri: product.image}}  style={styles.image}/>
+      <Image source={{uri: product.image ||  defaultImage }}  style={styles.image}/>
     <Text style={styles.title}>{product.name}</Text>
     <Text style={styles.price}>${product.price} </Text>
     </View>
